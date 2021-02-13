9cbab952f6ebc6f92328f29f0a33d9be92791957
Вацлав Довнар <v.dovnar@city-mobil.ru>
2021-02-13 11:12:53 +0300

    dev calculate wrt: calc criticality

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
 config = {
-    'project': {
-        'url': 'https://github.com/ClickHouse/ClickHouse'
-    }
+    'app_weight': {
+        'ClickHouse/ClickHouse': 12,
+        'authelia/authelia' : 10,
+        'gravitational/teleport' : 13
+    },
+    'defect_criticality': (
+    #(count_comments, criticality)
+        (11, 10),  # 12-infitly - Critical
+        (8, 5),  # 9-11 -Hight
+        (3, 2),  # 4-8  - medium
+        (0,   1)  #0-3  - low
+    )
 }
--- github_metrics.py
+++ github_metrics.py
@@ -1,9 +1,27 @@
+from github import Github
+import os
+
 from config import config
 
-def export_issues(github_repo_url):
-    return None
+def import_issues(github_repo_url):
+    repo = g.get_repo("ClickHouse/ClickHouse")
+    issues = repo.get_issues(labels=['security'], state='all')
+    issues = list(issues)
+    return issues
+
+def calculate_wrt(issues):
+    wrt = []
+    defect_criticality_dict = config['defect_criticality']
+    app_criticality = 0
+    for issue in issues:
+        for comment_count, criticality in defect_criticality_dict:
+            if issue.comments >= comment_count:
+                app_criticality += criticality
+
+    return criticality
 
 def search_security_issues(issues_list):
+    a=1
     return None
 
 def handle_issues_for_export(security_issues_list):
@@ -15,8 +33,15 @@ def export_to_mongo_db(result_data):
 def export_issues(result_data):
     export_to_mongo_db(result_data)
 
-all_issues = export_issues(config['config']['url'])
-security_issues = search_security_issues(all_issues)
+
+
+g = Github(os.environ['github_token'])
+
+#issues = import_issues(config['project']['url'])
+issues = import_issues('ClickHouse/ClickHouse')
+
+wrt = calculate_wrt(issues)
+security_issues = search_security_issues(issues)
 result_data = handle_issues_for_export(security_issues)
 export_status = export_issues(result_data)
 
